323f4fdf43fa8a34cdc5fc3171657aae45226181
PaddyK92 <Patrick.Klaiber@web.de>
2019-09-30 17:44:39 -0400

    Microphone Test

--- DeepSpeechAPI.py
+++ DeepSpeechAPI.py
@@ -1,5 +1,22 @@
 from deepspeech import Model
 import scipy.io.wavfile as wav
+from Recorder import record_audio
+import pyaudio
+
+#p = pyaudio.PyAudio()
+#for i in range(p.get_device_count()):
+#    dev = p.get_device_info_by_index(i)
+#    print((i, dev['name'], dev['maxInputChannels']))
+
+soundObj = pyaudio.PyAudio()
+
+# Learn what your OS+Hardware can do
+defaultCapability = soundObj.get_default_host_api_info()
+print(defaultCapability)
+
+# See if you can make it do what you want
+isSupported = soundObj.is_format_supported(input_format=pyaudio.paInt8, input_channels=1, rate=22050, input_device=0)
+print(isSupported)
 
 # https://progur.com/2018/02/how-to-use-mozilla-deepspeech-tutorial.html
 
@@ -7,6 +24,9 @@ import scipy.io.wavfile as wav
 model_path = "C:/Users/Patri/Downloads/DeepSpeech/models/output_graph.pb"
 alphabet_path = "C:/Users/Patri/Downloads/DeepSpeech/models/alphabet.txt"
 audio_path = "C:/Users/Patri/Downloads/DeepSpeech/audio/4507-16021-0012.wav"
+
+# load
+audio = record_audio()
 
 # load the model
 ds = Model(model_path, 26, 9, alphabet_path, 500)
