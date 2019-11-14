d8d9cc045fe2290891e9771fc28168b75f5a7b31
René Borner <mail@reneborner.de>
2019-11-13 20:32:12 -0500

    added recordButton

--- Plugin/EnglishToJava/src/main/java/MyToolWindow.java
+++ Plugin/EnglishToJava/src/main/java/MyToolWindow.java
@@ -1,12 +1,16 @@
 import com.intellij.openapi.wm.ToolWindow;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
 
 public class MyToolWindow {
     private JButton hideToolWindowButton;
     private JPanel myToolWindowContent;
+    private JButton recordButton;
 
-    public MyToolWindow(ToolWindow toolWindow) {
+    public MyToolWindow(ToolWindow toolWindow) throws IOException {
         hideToolWindowButton.addActionListener(e -> toolWindow.hide(null));
     }
 
--- Plugin/EnglishToJava/src/main/java/MyToolWindowFactory.java
+++ Plugin/EnglishToJava/src/main/java/MyToolWindowFactory.java
@@ -5,10 +5,17 @@ import com.intellij.ui.content.Content;
 import com.intellij.ui.content.ContentFactory;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.IOException;
+
 public class MyToolWindowFactory implements ToolWindowFactory {
     @Override
     public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
-        MyToolWindow myToolWindow = new MyToolWindow(toolWindow);
+        MyToolWindow myToolWindow = null;
+        try {
+            myToolWindow = new MyToolWindow(toolWindow);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         ContentFactory contentFactory = ContentFactory.SERVICE.getInstance();
         Content content = contentFactory.createContent(myToolWindow.getContent(), "", false);
         toolWindow.getContentManager().addContent(content);
