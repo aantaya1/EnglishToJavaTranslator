f9a00f8a7901e406a583aa7ff3e1f9bfc353d707
René Borner <mail@reneborner.de>
2019-11-16 19:38:57 -0500

    added soundrecorder and speechtotext with gpc

--- Plugin/EnglishToJava/src/main/java/MyToolWindow.java
+++ Plugin/EnglishToJava/src/main/java/MyToolWindow.java
@@ -1,20 +1,28 @@
 import com.intellij.openapi.wm.ToolWindow;
 
-import javax.imageio.ImageIO;
 import javax.swing.*;
-import java.awt.*;
 import java.io.IOException;
 
 public class MyToolWindow {
     private JButton hideToolWindowButton;
     private JPanel myToolWindowContent;
     private JButton recordButton;
+    private JLabel output;
 
     public MyToolWindow(ToolWindow toolWindow) throws IOException {
         hideToolWindowButton.addActionListener(e -> toolWindow.hide(null));
+        recordButton.addActionListener(e -> processRecording());
     }
 
     public JPanel getContent() {
         return myToolWindowContent;
+    }
+
+    public void processRecording(){
+        JavaSoundRecorder jsr = new JavaSoundRecorder();
+        jsr.startRecoding();
+        SpeechToText stt = new SpeechToText();
+        String textOutput = stt.getMockText();
+        output.setText(textOutput);
     }
 }
---
+++ Plugin/EnglishToJava/src/main/java/SpeechToText.java
@@ -0,0 +1,42 @@
+import com.google.cloud.speech.v1.RecognitionAudio;
+import com.google.cloud.speech.v1.RecognitionConfig;
+import com.google.cloud.speech.v1.RecognitionConfig.AudioEncoding;
+import com.google.cloud.speech.v1.RecognizeResponse;
+import com.google.cloud.speech.v1.SpeechClient;
+import com.google.cloud.speech.v1.SpeechRecognitionAlternative;
+import com.google.cloud.speech.v1.SpeechRecognitionResult;
+import com.google.protobuf.ByteString;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class SpeechToText {
+    public String getText(String fileName) throws Exception {
+        try (SpeechClient speechClient = SpeechClient.create()) {
+            Path path = Paths.get(fileName);
+            byte[] data = Files.readAllBytes(path);
+            ByteString audioBytes = ByteString.copyFrom(data);
+            RecognitionConfig config = RecognitionConfig.newBuilder()
+                    .setEncoding(AudioEncoding.LINEAR16)
+                    .setSampleRateHertz(16000)
+                    .setLanguageCode("en-US")
+                    .build();
+            RecognitionAudio audio = RecognitionAudio.newBuilder()
+                    .setContent(audioBytes)
+                    .build();
+            RecognizeResponse response = speechClient.recognize(config, audio);
+            List<SpeechRecognitionResult> results = response.getResultsList();
+            for (SpeechRecognitionResult result : results) {
+                SpeechRecognitionAlternative alternative = result.getAlternativesList().get(0);
+                System.out.printf("Transcription: %s%n", alternative.getTranscript());
+                return alternative.getTranscript();
+            }
+        }
+        return "no text recognized";
+    }
+
+    public String getMockText(){
+        return "this is a mock text";
+    }
+}
